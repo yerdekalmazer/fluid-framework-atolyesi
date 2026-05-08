d67a49949eaa0fd521d7d7e4a9f3afe89e50bb3c
yerdekalmazer <tyerdekalmazer01@gmail.com>
2026-05-08 17:13:23 +0300

    refactor: remove bug-app-react directory and all associated feature files

--- bug-app-react/src/shared/components/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-export function Avatar({ user, size = "md" }) {
-  const sizes = { sm: "w-8 h-7", md: "w-12 h-10", lg: "w-16 h-14" };
-  return (
-    <div className={`${sizes[size]} rounded-full ${user.color} text-white flex items-center justify-center font-bold text-sm shrink-0`}>
-      {user.initials}
-    </div>
-  );
-}
